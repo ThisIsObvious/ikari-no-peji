d1ecce854143c24076747cb8c0545828725e547b
ThisIsObvious <shvetsov@gmx.com>
2015-08-17 13:57:28 +0300

    Update urls.py

--- ikari/urls.py
+++ ikari/urls.py
@@ -20,4 +20,5 @@ urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
 	url(r'', include('blog.urls')),
 	url(r'^/login/$', 'django.contrib.auth.views.login'),
+	url(r'^/logout/$', 'django.contrib.auth.views.logout', {'next_page': '/'}),
 ]
