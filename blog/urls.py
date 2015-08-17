7162ffde1091be42f62c30c5e2abc47718efdb65
ThisIsObvious <shvetsov@gmx.com>
2015-08-17 13:16:17 +0300

    Update urls.py

--- blog/urls.py
+++ blog/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     url(r'^post/(?P<num>[0-9]+)/$', views.post_detail, name='post_detail'),
     url(r'^post/new/$', views.post_new, name='post_new'),
     url(r'^post/(?P<num>[0-9]+)/edit/$', views.post_edit, name='post_edit'),
-    url(r'^post/(?P<num>[0-9]+)/delete/$, views.post_delete, name='post_delete'),
+    url(r'^post/(?P<num>[0-9]+)/delete/$', views.post_delete, name='post_delete'),
 ]
