72a6a5692e55d5f986711b9eccd38f4f071b4b27
ThisIsObvious <shvetsov@gmx.com>
2015-08-17 11:53:43 +0300

    Update views.py

--- blog/views.py
+++ blog/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.utils import timezone
 from .models import Post
 from django.shortcuts import render, get_object_or_404
+from .forms import Note_form
 
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
@@ -10,3 +11,7 @@ def post_list(request):
 def post_detail(request, num):
     post = get_object_or_404(Post, pk=num)
     return render(request, 'blog/post_detail.html', {'post': post})
+
+def post_new(request):
+    form = Note_form()
+    return render(request, 'blog/post_edit.html', {'form': form})
