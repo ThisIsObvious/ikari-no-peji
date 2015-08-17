601fd9b81e818f534ba3915698d44288dd4b5e08
ThisIsObvious <shvetsov@gmx.com>
2015-08-17 13:48:18 +0300

    Update urls.py

--- blog/urls.py
+++ blog/urls.py
@@ -7,5 +7,4 @@ urlpatterns = [
     url(r'^post/new/$', views.post_new, name='post_new'),
     url(r'^post/(?P<num>[0-9]+)/edit/$', views.post_edit, name='post_edit'),
     url(r'^post/(?P<num>[0-9]+)/delete/$', views.post_delete, name='post_delete'),
-    url(r'^/login/$', 'django.contrib.auth.views.login'),
 ]
