3e53aa62c16b02d27c8d23cd4f94ee2e52528c18
ThisIsObvious <shvetsov@gmx.com>
2015-08-17 13:59:50 +0300

    Update urls.py

--- ikari/urls.py
+++ ikari/urls.py
@@ -19,6 +19,6 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
 	url(r'', include('blog.urls')),
-	url(r'^/login/$', 'django.contrib.auth.views.login'),
-	url(r'^/logout/$', 'django.contrib.auth.views.logout', {'next_page': '/'}),
+	url(r'^login/$', 'django.contrib.auth.views.login'),
+	url(r'^logout/$', 'django.contrib.auth.views.logout', {'next_page': '/'}),
 ]
