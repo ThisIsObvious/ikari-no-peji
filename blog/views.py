7df7a2ce139c6d647f12a393812b88dcc5dc955e
ThisIsObvious <shvetsov@gmx.com>
2015-08-17 11:37:17 +0300

    Update views.py

--- blog/views.py
+++ blog/views.py
@@ -7,6 +7,6 @@ def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
     
-def post_detail(request, id):
-    post = get_object_or_404(Post, id=id)
+def post_detail(request, num):
+    post = get_object_or_404(Post, pk=num)
     return render(request, 'blog/post_detail.html', {'post': post})
