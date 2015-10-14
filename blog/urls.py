b143ea97ab29cfa6b9f6a298ace514badb147384
ThisIsObvious <shvetsov@gmx.com>
2015-10-14 16:43:01 +0300

    Update urls.py

--- blog/urls.py
+++ blog/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     url(r'^post/new/$', views.post_new, name='post_new'),
     url(r'^post/(?P<num>[0-9]+)/edit/$', views.post_edit, name='post_edit'),
     url(r'^post/(?P<num>[0-9]+)/delete/$', views.post_delete, name='post_delete'),
-    url(r'^manga/(?P<num>[0-9]+)/
+    url(r'^manga/(?P<num>[0-9]+)/$',view.manga_general, name='manga_general'),
+    url(r'^manga/(?P<num>[0-9]+)/page/(?P<num>[0-9]+)/$', view.manga_page, name='manga_page'),
 ]
