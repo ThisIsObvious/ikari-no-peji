6012cd8d9ce4b981e9e4a936533ea82052ce6dfe
ThisIsObvious <shvetsov@gmx.com>
2015-10-14 19:04:36 +0300

    Update models.py

--- blog/models.py
+++ blog/models.py
@@ -34,7 +34,7 @@ class Page(models.Model):
         default = 0
     )
     page = models.ImageField(
-        upload_to='ikari-no-peji/pages/', 
+        upload_to='ikari-no-peji/', 
         height_field='height', 
         width_field='width'
     )
