193b7e55c247194d41ab1d2ee80877b769a466f2
ThisIsObvious <shvetsov@gmx.com>
2015-10-19 17:39:22 +0300

    Update autopopulate.py

--- autopopulate.py
+++ autopopulate.py
@@ -8,15 +8,15 @@ from urllib.parse import urlparse
 
 
 def add_page(num):
-    p = Page.objects.get_or_create(number=num)[num]
-        p.url = 'http://img.mangastream.com/cdn/manga/53/2962/' + str(0) + str(num) +'.png'
-        r = requests.get(p.url, stream=True)
-        with open(str(0)+str(num)+'.png', 'wb') as f:
-            r.raw.decode_content = True
-            shutil.copyfileobj(r.raw, f)
-            p.page.save(str(0)+str(num)+'.png', f, save=True)
-        del response
-        p.caption= str(num*2) + str(User.objects.all())
+    p = Page.objects.get_or_create(number=num)
+    p.url = 'http://img.mangastream.com/cdn/manga/53/2962/' + str(0) + str(num) +'.png'
+    r = requests.get(p.url, stream=True)
+    with open(str(0)+str(num)+'.png', 'wb') as f:
+        r.raw.decode_content = True
+        shutil.copyfileobj(r.raw, f)
+        p.page.save(str(0)+str(num)+'.png', f, save=True)
+    del response
+    p.caption= str(num*2) + str(User.objects.all())
     return p
 
 if __name__ == "__main__":
