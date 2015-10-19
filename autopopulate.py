f4d21b437b4fe3b61f68750d4b956ae3d0bff544
ThisIsObvious <shvetsov@gmx.com>
2015-10-19 17:37:03 +0300

    Update autopopulate.py

--- autopopulate.py
+++ autopopulate.py
@@ -1,18 +1,25 @@
-number = [str(i) for i in range(1,17)]
-URL = str(http://img.mangastream.com/cdn/manga/53/2962/) + str(0) + [str(i) for i in range(1,17)]
-caption = [str(i) for i in range(2,34, 2)]
-manga = Bleach
-
 import os
-os.environ['DJANGO_SETTINGS_MODULE']='project.settings'
-
-from customer.models import Customer
+import shutil
+import requests
+from .models import Manga, Page
 from django.contrib.auth.models import User
+from django.core.files import File
+from urllib.parse import urlparse
 
-users = User.objects.all()
+
+def add_page(num):
+    p = Page.objects.get_or_create(number=num)
+        p.url = 'http://img.mangastream.com/cdn/manga/53/2962/' + str(0) + str(num) +'.png'
+        r = requests.get(p.url, stream=True)
+        with open(str(0)+str(num)+'.png', 'wb') as f:
+            r.raw.decode_content = True
+            shutil.copyfileobj(r.raw, f)
+            p.page.save(str(0)+str(num)+'.png', f, save=True)
+        del response
+        p.caption= str(num*2) + str(User.objects.all())
+    return p
 
 if __name__ == "__main__":
+    print "Script..."
     for i in range(10):
-        customer = Customer(first_name=choice(first_names), last_name=choice(last_names),
-                        telephone=choice(phones),email=choice(emails), creator=choice(users))
-        customer.save()
+        add_page(i)
