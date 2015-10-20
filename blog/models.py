25558e41fcda08bca4427bebd95404ef4825f503
ThisIsObvious <shvetsov@gmx.com>
2015-10-20 17:51:59 +0300

    Update models.py

--- blog/models.py
+++ blog/models.py
@@ -7,6 +7,28 @@ from PIL import Image
 import imghdr
 import copy
 
+def download_image(url):
+    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 5.1; rv:31.0) Gecko/20100101 Firefox/31.0'}
+    r = Request(url, headers=headers)
+    request = Request.urlopen(r, timeout=10)
+    image_data = StringIO(request.read())
+    img = Image.open(image_data)
+    img_copy = copy.copy(img)
+    if valid_img(img_copy):
+        return img
+    else:
+        raise Exception('An invalid image was detected when attempting to save a Page!')
+
+def valid_img(img):
+    type = img.format
+    if type in ('GIF', 'JPEG', 'JPG', 'PNG'):
+        try:
+            img.verify()
+            return True
+        except:
+            return False
+    else: return False
+
 
 class Post(models.Model):
     author = models.ForeignKey('auth.User')
@@ -66,25 +88,3 @@ class Page(models.Model):
         super(Page, self).save(*args, **kwargs)
     def __str__(self):
         return str(self.number)
-
-def download_image(url):
-    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 5.1; rv:31.0) Gecko/20100101 Firefox/31.0'}
-    r = Request(url, headers=headers)
-    request = Request.urlopen(r, timeout=10)
-    image_data = StringIO(request.read())
-    img = Image.open(image_data)
-    img_copy = copy.copy(img)
-    if valid_img(img_copy):
-        return img
-    else:
-        raise Exception('An invalid image was detected when attempting to save a Page!')
-
-def valid_img(img):
-    type = img.format
-    if type in ('GIF', 'JPEG', 'JPG', 'PNG'):
-        try:
-            img.verify()
-            return True
-        except:
-            return False
-    else: return False
