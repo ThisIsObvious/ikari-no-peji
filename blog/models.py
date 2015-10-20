edd8c9f8508dc0f93e140e17073de40b3bc6d0d3
ThisIsObvious <shvetsov@gmx.com>
2015-10-20 17:23:58 +0300

    Update models.py

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,11 @@
 from django.db import models
 from django.utils import timezone
+from io import StringIO
+from urllib import parse
+from urllib import request
+from pillow import image
+import imghdr
+import copy
 
 
 class Post(models.Model):
@@ -40,20 +46,45 @@ class Page(models.Model):
         upload_to='.',
         null=True, 
         blank=True,
-        height_field='height', 
-        width_field='width'
     )
     caption = models.TextField(blank=True)
     manga = models.ForeignKey('Manga')
-    width = models.PositiveIntegerField(
-        blank = True, null = True,
-        editable = False,
-        default = 0
-    )
-    height = models.PositiveIntegerField(
-        blank = True, null = True,
-        editable = False,
-        default = 0
-    )
+
+    def save(self, url='', *args, **kwargs):
+        if self.prod_img != '' and url != '':
+            image = download_image(url)
+            try:
+                filename = urlparse.urlparse(url).path.split('/')[-1]
+                self.prod_img = filename
+                tempfile = image
+                tempfile_io = StringIO()
+                tempfile.save(tempfile_io, format=image.format)
+                self.prod_img.save(filename, ContentFile(tempfile_io.getvalue()), save=False) 
+            except Exception, e:
+                print ("Error trying to save model: saving image failed: " + str(e))
+                pass
+        super(Product, self).save(*args, **kwargs)
     def __str__(self):
         return str(self.number)
+
+def download_image(url):
+    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 5.1; rv:31.0) Gecko/20100101 Firefox/31.0'}
+    r = Request(url, headers=headers)
+    request = Request.urlopen(r, timeout=10)
+    image_data = StringIO(request.read())
+    img = Image.open(image_data)
+    img_copy = copy.copy(img)
+    if valid_img(img_copy):
+        return img
+    else:
+        raise Exception('An invalid image was detected when attempting to save a Product!')
+
+def valid_img(img):
+    type = img.format
+    if type in ('GIF', 'JPEG', 'JPG', 'PNG'):
+         try:
+            img.verify()
+            return True
+        except:
+            return False
+    else: return False
