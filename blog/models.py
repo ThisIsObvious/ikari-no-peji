ea9896fa99b56a8d2cbe3a8d2787b2f5345290e3
ThisIsObvious <shvetsov@gmx.com>
2015-10-20 17:54:53 +0300

    Update models.py

--- blog/models.py
+++ blog/models.py
@@ -9,7 +9,7 @@ import copy
 
 def download_image(url):
     headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 5.1; rv:31.0) Gecko/20100101 Firefox/31.0'}
-    r = Request(url, headers=headers)
+    r = request(url, headers=headers)
     request = request.urlopen(r, timeout=10)
     image_data = StringIO(request.read())
     img = Image.open(image_data)
