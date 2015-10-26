5e7829f9d5809724ae53aaaba9f2f8e85705d3f9
ThisIsObvious <shvetsov@gmx.com>
2015-10-26 10:15:37 +0300

    Update urls.py

--- blog/urls.py
+++ blog/urls.py
@@ -11,6 +11,7 @@ urlpatterns = [
     url(r'^post/(?P<num>[0-9]+)/delete/$', views.post_delete, name='post_delete'),
     url(r'^manga/(?P<num>[0-9]+)/$',views.manga_general, name='manga_general'),
     url(r'^(?P<manga>[0-9]+)/(?P<numb>[0-9]+)/$', views.manga_page, name='manga_page'),
+    url(r'^shop/$', view.shop, name='shop')
 ] 
 if settings.DEBUG:
     urlpatterns += patterns('django.views.static',
